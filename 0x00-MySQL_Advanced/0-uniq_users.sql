5c17dbb18c9c7e13e1c076f62a5a45b613bb884c
tamerkat <mahad10taha@gmail.com>
2024-08-13 13:54:01 +0300

    add

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,4 @@
+-- Check if the table already exists
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- table
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTOINCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+)
