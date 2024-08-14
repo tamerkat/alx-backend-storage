1c3ed294e3f7934d73132ab286697654a53364a3
tamerkat <mahad10taha@gmail.com>
2024-08-14 15:13:40 +0300

    add

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
 
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Optional
 
 
 class Cache:
@@ -19,3 +19,19 @@ class Cache:
         key = str(uuid4())
         self._redis.mset({key: data})
         return key
+
+    def get(self, key: str, fn: Optional[callable[[bytes], Union[str, float, bytes, int]]] = None) -> Union[int, float, bytes, None, str]:
+        data = self.get(key)
+
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        return self.get(key, lambda data: data.decode('utf-8'))
+
+    def get_int(self, key: int) -> Optional[int]:
+        return self.get(key, lambda data: int(data))
